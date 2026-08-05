e9952b034b720ee6522fb54f04dcfb30ab42cb4c
PereyraHebe <p_hebe_c@hotmail.com>
2026-08-05 18:30:28 -0300

    Se crea estructura de carpeta, segun la documentacion realizada.

---
+++ frontend/granjanv/src/app/feactures/dashboard/panel-produccion/gestion-huevos/gestion-huevos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-huevos',
+  imports: [],
+  templateUrl: './gestion-huevos.html',
+  styleUrl: './gestion-huevos.css',
+})
+export class GestionHuevos {}
