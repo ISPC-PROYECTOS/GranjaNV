d350240c787fc75f594266dc49151b6e372854fb
MarielarosaSZ <mariela_suarezz@hotmail.com>
2026-08-08 19:31:33 -0300

    incorporación de diseño y lógica del login

--- frontend/granjanv/src/app/app.routes.ts
+++ frontend/granjanv/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [// Ruta pública por defecto
+  {
+    path: '',
+    redirectTo: 'auth/auth',
+    pathMatch: 'full'
+  },
+  /*{
+    path: 'public',
+    loadComponent: () => import('./feactures/public/public').then(m => m.PublicComponent)
+  }, */
+
+  // Vista de Login
+  {
+    path: 'auth/auth',
+    loadComponent: () => import('./feactures/auth/auth').then(m => m.LoginComponent)
+  },
+{
+    path: '**',
+    redirectTo: 'auth/login'
+  }
+];
