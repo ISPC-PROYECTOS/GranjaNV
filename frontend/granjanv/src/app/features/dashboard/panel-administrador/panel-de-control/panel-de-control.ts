362b381be219f5c22faa30db99ac7b277b9d58c0
JMarchisone <jorgemarchisone@gmail.com>
2026-08-09 13:51:00 -0300

    Merge con rama de mariela

---
+++ frontend/granjanv/src/app/features/dashboard/panel-administrador/panel-de-control/panel-de-control.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-de-control',
+  imports: [],
+  templateUrl: './panel-de-control.html',
+  styleUrl: './panel-de-control.css',
+})
+export class PanelDeControl {}
---
+++ frontend/granjanv/src/app/features/dashboard/panel-produccion/gestion-gallinas/gestion-gallinas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-gallinas',
+  imports: [],
+  templateUrl: './gestion-gallinas.html',
+  styleUrl: './gestion-gallinas.css',
+})
+export class GestionGallinas {}
