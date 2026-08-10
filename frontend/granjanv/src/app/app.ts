3ba69de3be3209d4c6efccc9c0af39e48b8d2cf3
Victoria Picco <vpicco1996@gmail.com>
2026-08-10 15:47:23 -0300

    [FEAT]: Maquetado responsive dashboard admistrador

--- frontend/granjanv/src/app/app.ts
+++ frontend/granjanv/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
+import { PanelDeControl } from "./feactures/dashboard/panel-administrador/panel-de-control/panel-de-control";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PanelDeControl],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
