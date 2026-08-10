49de2294f806012dade6d0c8b59e22e46c5d06cd
JMarchisone <jorgemarchisone@gmail.com>
2026-08-09 23:14:22 -0300

    Se quita url en navbar que daba multiples errores en consola. Se corrige settings de backend y url.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -134,6 +134,8 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id_usuario',
+    'USER_ID_CLAIM': 'user_id',
 }
 
 
@@ -153,3 +155,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+#Mock email backend for development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
