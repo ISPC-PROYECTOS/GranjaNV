bc8b04211d9c84a616148edca70e9b2a2c110f13
PereyraHebe <p_hebe_c@hotmail.com>
2026-08-10 21:37:37 -0300

    se agrega ruteo y nevbar

--- frontend/granjanv/src/app/app.routes.ts
+++ frontend/granjanv/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/auth';
 import { RegistroUsuarioComponent } from './features/dashboard/panel-administrador/registro-usuario/registro-usuario';
 import { adminGuard } from './core/guards/admin-guard';
-
+// 🟢 Agregamos la comilla ' que faltaba al final del path
+import { PanelDeControl } from './features/dashboard/panel-administrador/panel-de-control/panel-de-control';
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
+
+  // 🟢 Ruta de Victoria (coincide con la que invoca auth.ts al loguear como Admin)
+  { 
+    path: 'dashboard/admin/panel-de-control', 
+    component: PanelDeControl 
+  },
   {
     path: 'dashboard/admin/registro-usuario',
     component: RegistroUsuarioComponent,
