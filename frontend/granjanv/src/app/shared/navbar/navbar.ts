6f31446de677a115322d2edaf1be852e72cdd632
PereyraHebe <p_hebe_c@hotmail.com>
2026-08-12 21:04:01 -0300

    Feat: Se agregan iniciales dinámica en el ávatar del Navbar

--- frontend/granjanv/src/app/shared/navbar/navbar.ts
+++ frontend/granjanv/src/app/shared/navbar/navbar.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from "@angular/router";
-import { AuthService } from '../../core/services/auth-service'; 
+import { AuthService } from '../../core/services/auth-service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +15,16 @@ export class NavbarComponent implements OnInit {
   private router = inject(Router);
   private authService = inject(AuthService);
 
-  usuarioInicial: string = 'N';
+  // Inicial dinámica reactiva mediante Signal computed
+  usuarioInicial = computed(() => {
+    const u = this.authService.currentUser();
+    if (!u) return 'U';
+
+    return (u.nombre && u.apellido) 
+      ? `${u.nombre[0]}${u.apellido[0]}`.toUpperCase() 
+      : (u.email ? u.email[0].toUpperCase() : 'U');
+  });
+
   climaInfo: string = '18° Parcialmente nublado';
   fechaActual: string = '';
 
@@ -33,11 +42,12 @@ export class NavbarComponent implements OnInit {
     this.fechaActual = hoy.charAt(0).toUpperCase() + hoy.slice(1);
   }
 
-  // Método  verificar la URL actual
+  // Método para verificar la URL actual
   esVistaLogin(): boolean {
     return this.router.url.includes('/auth/login');
   }
 
+  // Método para cerrar sesión vinculado al botón
   cerrarSesion(): void {
     this.authService.logout();
   }
