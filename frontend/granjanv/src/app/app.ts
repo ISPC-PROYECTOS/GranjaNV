8ee6e06647a59d1710b087c5ebe89adbc3c58fe6
JMarchisone <jorgemarchisone@gmail.com>
2026-08-14 11:35:25 -0300

    [FEAT] Primera implementacion de modelo y servicio de api de clima

--- frontend/granjanv/src/app/app.ts
+++ frontend/granjanv/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
-import { PanelDeControl } from "./features/dashboard/panel-administrador/panel-de-control/panel-de-control";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PanelDeControl],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontend/granjanv/src/app/core/models/weather.ts
@@ -0,0 +1,23 @@
+export interface WeatherData {
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+  windDirection: number;        // Grados (0° - 360°)
+  windDirectionText: string;    // N, NE, E, SE, S, SO, O, NO
+  precipitationProbability: number;
+  description: string;
+  iconClass: string;
+}
+
+export interface OpenMeteoResponse {
+  current: {
+    temperature_2m: number;
+    relative_humidity_2m: number;
+    weather_code: number;
+    wind_speed_10m: number;
+    wind_direction_10m: number;
+  };
+  hourly: {
+    precipitation_probability: number[];
+  };
+}
