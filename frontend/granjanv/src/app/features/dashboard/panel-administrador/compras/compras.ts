a1ad8696d511cabfdcdf9d353961f3e137a3878a
Victoria Picco <vpicco1996@gmail.com>
2026-08-18 23:02:08 -0300

    [FEAT]: Agregado de navegación entre panel de control y finanzas

--- frontend/granjanv/src/app/features/dashboard/panel-administrador/compras/compras.ts
+++ frontend/granjanv/src/app/features/dashboard/panel-administrador/compras/compras.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Gasto } from '../../../../core/models/gasto.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-compras',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './compras.html',
   styleUrl: './compras.css',
 })
--- frontend/granjanv/src/app/features/dashboard/panel-administrador/panel-de-control/panel-de-control.ts
+++ frontend/granjanv/src/app/features/dashboard/panel-administrador/panel-de-control/panel-de-control.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { routes } from '../../../../app.routes';
 
 @Component({
   selector: 'app-panel-de-control',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './panel-de-control.html',
   styleUrl: './panel-de-control.css',
 })
