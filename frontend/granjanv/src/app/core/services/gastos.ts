6a36d012345d6c8d9fd39875d9b8273247e7ec0e
Victoria Picco <vpicco1996@gmail.com>
2026-08-19 21:37:35 -0300

    [FEAT]: Implementación de CRUD de gastos en finanzas

---
+++ frontend/granjanv/src/app/core/services/gastos.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Gasto } from '../models/gasto.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Gastos {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8000/api/compras/gastos/';
+
+  obtenerGastos() {
+    return this.http.get<Gasto[]>(this.apiUrl);
+  }
+
+  crearGasto(gasto: Partial<Gasto>) {
+    return this.http.post<Gasto>(this.apiUrl, gasto);
+  }
+
+  eliminarGasto(id: number) {
+    return this.http.delete(`${this.apiUrl}${id}/`);
+  }
+
+  actualizarGasto(id: number, gasto: Partial<Gasto>) {
+    return this.http.patch<Gasto>(`${this.apiUrl}${id}/`, gasto);
+  }
+}
