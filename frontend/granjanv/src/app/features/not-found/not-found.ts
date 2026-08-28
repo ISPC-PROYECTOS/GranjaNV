5a92cc246f8501a858d0263c1621a4b65a031a7e
JMarchisone <jorgemarchisone@gmail.com>
2026-08-27 23:33:55 -0300

    Fix(not-found) se quita RouterLink del NotFound por no utilizarse.

--- frontend/granjanv/src/app/features/not-found/not-found.ts
+++ frontend/granjanv/src/app/features/not-found/not-found.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth-service';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css'
 })
