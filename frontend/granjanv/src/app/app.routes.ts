1d685bbc4750e730d3025966238e3e8ed7df33bc
JMarchisone <jorgemarchisone@gmail.com>
2026-08-27 23:29:28 -0300

    Feat(UX/UI) Se agrega página de error 404, que redirige dependiendo si el usuario esta logeado o no, y si es admin o empleado

--- frontend/granjanv/src/app/app.routes.ts
+++ frontend/granjanv/src/app/app.routes.ts
@@ -3,14 +3,13 @@ import { LoginComponent } from './features/auth/auth';
 import { RegistroUsuarioComponent } from './features/dashboard/panel-administrador/registro-usuario/registro-usuario';
 import { adminGuard } from './core/guards/admin-guard';
 import { authGuard } from './core/guards/auth-guard';
-// 🟢 Agregamos la comilla ' que faltaba al final del path
 import { PanelDeControl } from './features/dashboard/panel-administrador/panel-de-control/panel-de-control';
 import { Compras } from './features/dashboard/panel-administrador/compras/compras';
+import { NotFoundComponent } from './features/not-found/not-found';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
 
-  // 🟢 Ruta de Victoria (coincide con la que invoca auth.ts al loguear como Admin)
   { 
     path: 'dashboard/admin/panel-de-control', 
     component: PanelDeControl,
@@ -27,5 +26,5 @@ export const routes: Routes = [
     canActivate: [adminGuard]
   },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth/login' }
+  { path: '**', component: NotFoundComponent }
 ];
