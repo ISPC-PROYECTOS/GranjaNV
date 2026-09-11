9d9327dfe7d0fc4ef7e65f1191e47cf6d7ddc148
PereyraHebe <p_hebe_c@hotmail.com>
2026-09-11 16:23:25 -0300

    Refactor (modelo cliente): se agregan validaciones para nombre, apellido y teléfono

--- frontend/granjanv/src/app/app.ts
+++ frontend/granjanv/src/app/app.ts
@@ -2,13 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
 
-/**Solo se prueba, antes de integrar el form a la vista pedidos */
-import { CrearClienteComponent } from './shared/clientes/crear-cliente';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CrearClienteComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
